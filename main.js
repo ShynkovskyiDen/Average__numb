1174b295cd3c9a42928fb804a88a5907b58f481e
ShynkovskyiDen <shinkovskiy.dx@gmail.com>
2023-11-16 21:04:53 +0200

    added file average__numb

---
+++ main.js
@@ -0,0 +1,5 @@
+let firstNumb = +prompt('First number');
+let secondNumb = +prompt('Second number');
+let thirdNumb = +prompt('Third number');
+
+alert(`(${firstNumb} + ${secondNumb} + ${thirdNumb}) / 3 = ${(firstNumb + secondNumb + thirdNumb) / 3}`);
